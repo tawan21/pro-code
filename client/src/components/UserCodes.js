87ce99ac0a8b05c5e52933aee6a8d2d903f4f6a5
Tawanjot Singh <tawanjot_singh@yahoo.com>
2023-03-22 11:34:42 +0530

    final commit

--- client/src/components/UserCodes.js
+++ client/src/components/UserCodes.js
@@ -40,16 +40,16 @@ const UserCodes = () => {
   return (
     <div className='flex flex-col p-5 space-y-5'>
       <Link to='/landing' className='text-center border-2 border-black z-10 rounded-md shadow-[4px_4px_0px_0px_rgba(0,0,0)] px-4 py-2 flex-shrink-0 bg-yellow-500 text-black hover:shadow transition duration-150 text-xl w-fit'>Back</Link>
-      <div className='grid grid-cols-1 md:grid-cols-5 gap-2'>
+      <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-2'>
         {codes.map((code) => <CodeCard key={code._id} title={code.title} code={code.code} date={code.date} id={code._id} children={<div className="flex justify-evenly">
           <Link to="/landing" state={{
             id: code._id,
             title: code.title,
             code: code.code
-          }} className="inline-flex items-center px-3 py-2 text-xs lg:text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          }} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Edit
           </Link>
-          <button type="button" className="inline-flex items-center px-3 py-2 text-xs lg:text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800" onClick={() => deleteCode(code._id)}>
+          <button type="button" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800" onClick={() => deleteCode(code._id)}>
             Delete
           </button>
         </div>} />)}
